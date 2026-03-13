d82ea39ac1830c23277a4186d412e0b185f8dd82
Dalton Pacola Franco <devpacola@hotmail.com>
2026-03-13 00:27:50 -0300

    feat: Criacao da repository dos pontos turisticos

---
+++ API/TesteWebEvertec/Repositories/PontosTuristicosRepository.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using TesteWebEvertec.Data;
+using TesteWebEvertec.Models;
+
+namespace TesteWebEvertec.Repositories;
+
+public class PontosTuristicosRepository(AppDbContext appDbContext)
+{
+    private readonly AppDbContext _dbContext = appDbContext;
+
+    public async Task<PontoTuristico> Inserir(PontoTuristico pontoTuristico)
+    {
+        await _dbContext.PontosTuristicos.AddAsync(pontoTuristico);
+        await _dbContext.SaveChangesAsync();
+        return pontoTuristico;
+    }
+
+    public async Task<List<PontoTuristico>> BuscarTodos()
+    {
+        return await _dbContext.PontosTuristicos
+            .Where(p => p.DeletadoEm == null)
+            .ToListAsync();
+    }
+
+    public async Task<List<PontoTuristico>> BuscarTodosPorCategoria(string categoria)
+    {
+        return await _dbContext.PontosTuristicos
+            .Where(p => p.Categoria.Equals(categoria) && p.DeletadoEm == null)
+            .ToListAsync();
+    }
+
+    public async Task<PontoTuristico?> BuscarPorId(int id)
+    {
+        return await _dbContext.PontosTuristicos
+            .FirstOrDefaultAsync(p => p.Id == id && p.DeletadoEm == null);
+    }
+}
