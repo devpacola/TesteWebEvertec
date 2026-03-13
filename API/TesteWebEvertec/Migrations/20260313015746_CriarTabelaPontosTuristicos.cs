33174983e98df0c0c041fd3e7e204e19e1140b2e
Dalton Pacola Franco <devpacola@hotmail.com>
2026-03-13 00:30:21 -0300

    feat: Criacao da migration da criacao da tabela tb_pontos_turisticos

---
+++ API/TesteWebEvertec/Migrations/20260313015746_CriarTabelaPontosTuristicos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteWebEvertec.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaPontosTuristicos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_pontos_turisticos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Endereco = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Cidade = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Estado = table.Column<string>(type: "TEXT", maxLength: 2, nullable: false),
+                    Categoria = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "datetime('now', 'localtime')"),
+                    AtualizadoEm = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "datetime('now', 'localtime')"),
+                    DeletadoEm = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_pontos_turisticos", x => x.Id);
+                    table.CheckConstraint("CK_PontoTuristico_Categoria", "Categoria IN ('Praia', 'Parque', 'Museu', 'Monumento', 'Gastronomia', 'Outro')");
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_pontos_turisticos");
+        }
+    }
+}
